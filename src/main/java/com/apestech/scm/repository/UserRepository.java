074dcf26c6cabad3a2de9b70c23ce7d2abd15428
xuliang87 <xl@jl-soft.com>
2017-12-16 09:26:20 +0800

    删除

--- src/main/java/com/apestech/scm/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apestech.scm.repository;
-
-import com.apestech.scm.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-
-	
-}
